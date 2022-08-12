f992e952609b8241071e0e5007e2b72a7e14a6c9
Yusif Bayramov <byusub2@gmail.com>
2022-08-12 06:31:18 +0400

    menu-ic-seyfe edit

--- js/circular-slider.js
+++ js/circular-slider.js
@@ -1,3 +1,38 @@
+let dropdown = document.querySelector(".dropbtn");
+let mydropdown = document.querySelector(".dropdown-content");
+
+dropdown.addEventListener("click", () => {
+    mydropdown.classList.toggle("show")
+    dropdown.classList.toggle("active")
+});
+const menuBtn = document.querySelector(".menu-icon span");
+const searchBtn = document.querySelector(".search-icon");
+const cancelBtn = document.querySelector(".cancel-icon");
+const items = document.querySelector(".nav-items");
+const form = document.querySelector("form");
+
+menuBtn.onclick = () => {
+    items.classList.add("active");
+    menuBtn.classList.add("hide");
+    searchBtn.classList.add("hide");
+    cancelBtn.classList.add("show");
+}
+
+cancelBtn.onclick = () => {
+    items.classList.remove("active");
+    menuBtn.classList.remove("hide");
+    searchBtn.classList.remove("hide");
+    cancelBtn.classList.remove("show");
+    cancelBtn.style.color = "#ff3d00"
+    form.classList.remove("active");
+}
+searchBtn.onclick = () => {
+    form.classList.add("active");
+    searchBtn.classList.add("hide");
+    cancelBtn.classList.add("show");
+}
+
+
 (function() {
     'use strict';
 
@@ -226,36 +261,3 @@
     //////////////////////
 
 })();
-let dropdown = document.querySelector(".dropbtn");
-let mydropdown = document.querySelector(".dropdown-content");
-
-dropdown.addEventListener("click", () => {
-    mydropdown.classList.toggle("show")
-    dropdown.classList.toggle("active")
-});
-const menuBtn = document.querySelector(".menu-icon span");
-const searchBtn = document.querySelector(".search-icon");
-const cancelBtn = document.querySelector(".cancel-icon");
-const items = document.querySelector(".nav-items");
-const form = document.querySelector("form");
-
-menuBtn.onclick = () => {
-    items.classList.add("active");
-    menuBtn.classList.add("hide");
-    searchBtn.classList.add("hide");
-    cancelBtn.classList.add("show");
-}
-
-cancelBtn.onclick = () => {
-    items.classList.remove("active");
-    menuBtn.classList.remove("hide");
-    searchBtn.classList.remove("hide");
-    cancelBtn.classList.remove("show");
-    cancelBtn.style.color = "#ff3d00"
-    form.classList.remove("active");
-}
-searchBtn.onclick = () => {
-    form.classList.add("active");
-    searchBtn.classList.add("hide");
-    cancelBtn.classList.add("show");
-}
